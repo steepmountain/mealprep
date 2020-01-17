1b35a8a21d64f2a8cfd7a9549b071625c60f65b8
Ruben Bratsbergm <rubenbratsberg@gmail.com>
2020-01-17 19:04:36 +0100

    Working MVP

--- src/components/IngredientList.vue
+++ src/components/IngredientList.vue
@@ -1,72 +1,73 @@
 <template>
-<div class="ingredient-list">
+  <div class="ingredient-list">
     <cv-form id="meal-form">
-        <IngredientRow v-for="item in items" v-bind:key="item.id" v-bind:index="item.id" v-bind:name="item.name" v-bind:amount="item.amount" v-bind:unit="item.unit" v-bind:caloriesPerAmount="item.caloriesPerAmount" v-bind:totalCalories="item.totalCalories" v-on:change="updateCalories" />
+      <IngredientRow
+        v-for="item in items"
+        v-bind:key="item.id"
+        v-bind:ingredientIn="item"
+        v-on:change="update"
+      />
     </cv-form>
     Total calories: {{sumCalories}}
     <cv-button @click="addRow">Ny rad</cv-button>
-</div>
+  </div>
 </template>
 
 <script>
 import IngredientRow from "./IngredientRow.vue";
 // Todo: make a model of row and send it down. edit the model in the row and see the edits up here
 export default {
-    name: "app",
-    data() {
-        return {
-            sumCalories: 0,
-            currentIndex: 1,
-            items: []
-        };
-    },
-    components: {
-        IngredientRow
-    },
-    mounted: function () {
-        this.addRow();
-    },
-    // computed: {
-    //     summedCalories: function () {
-    //         return this.sumCalories = this.items.reduce((sum, child) => {
-    //             sum + child.rowCalories;
-    //         });
-    //     }
-    // },
-    methods: {
-        updateCalories() {
-            this.sumCalories = this.items
-                .map(item => item.totalCalories)
-                .reduce((sum, next) => {
-                    sum + next;
-                }, 0);
-        },
+  name: "app",
+  data() {
+    return {
+      sumCalories: 0,
+      currentIndex: 1,
+      items: []
+    };
+  },
+  components: {
+    IngredientRow
+  },
+  mounted: function() {
+    this.addRow();
+  },
+  methods: {
+    update(mutated) {
+      let current = this.items.filter(item => item.index == mutated.index);
+      let itemIndex = this.items.indexOf(current);
+      let updated = { ...current, ...mutated };
+      this.items.splice(itemIndex, 1, updated);
 
-        addRow() {
-            this.currentIndex++;
-            let ingredient = {
-                key: this.currentIndex,
-                index: this.currentIndex,
-                name: "",
-                amount: 0,
-                unit: "",
-                caloriesPerAmount: 0,
-                totalCalories: 0
-            };
-            this.items.push(ingredient);
-        }
+      this.sumCalories = this.items
+        .map(item => item.totalCalories)
+        .reduce((sum, next) => sum + next, 0);
+    },
+
+    addRow() {
+      this.currentIndex++;
+      let ingredient = {
+        key: this.currentIndex,
+        index: this.currentIndex,
+        name: "",
+        amount: 0,
+        unit: "",
+        caloriesPerAmount: 0,
+        totalCalories: 0
+      };
+      this.items.push(ingredient);
     }
+  }
 };
 </script>
 
 <style scoped>
 .ingredient-row {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 2rem;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 2rem;
 }
 
 .bx--form-item {
-    margin: 0 40px;
+  margin: 0 40px;
 }
 </style>
--- src/components/IngredientRow.vue
+++ src/components/IngredientRow.vue
@@ -1,51 +1,74 @@
 <template>
-<div class="ingredient-row">
-    <cv-text-input label="Ingrediens" v-model="name" placeholder="Ingrediens" />
+  <div class="ingredient-row">
+    <cv-text-input label="Ingrediens" v-model="ingredient.name" placeholder="Ingrediens" />
 
-    <cv-number-input label="Mengde" v-model="amount" min="0" @input="calulcateCalories" />
+    <cv-number-input label="Mengde" v-model="ingredient.amount" min="0" @input="calulcateCalories" />
 
-    <cv-text-input label="Måleenhet" v-model="unit" placeholder="Måleenhet" />
+    <cv-text-input
+      label="Måleenhet"
+      v-model="ingredient.unit"
+      placeholder="Måleenhet"
+      @input="$emit('update-unit', $event.target.value)"
+    />
 
-    <cv-number-input label="Kalorier per måleenhet" v-model="caloriesPerAmount" min="0" @input="calulcateCalories" />
-    {{totalCalories}}
-</div>
+    <cv-number-input
+      label="Kalorier per måleenhet"
+      v-model="ingredient.caloriesPerAmount"
+      min="0"
+      @input="calulcateCalories"
+    />
+    {{ingredient.totalCalories}}
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        index: Number,
-        name: String,
-        amount: Number,
-        unit: String,
-        caloriesPerAmount: Number,
-        totalCalories: Number,
-    },
-    data() {
-        return {
-        };
-    },
-    methods: {
-        calulcateCalories() {
-            if (this.caloriesPerAmount > 0 && this.amount > 0) {
-                this.totalCalories = this.caloriesPerAmount * this.amount;
-            } else {
-                this.totalCalories = 0;
-            }
-            this.$emit('change');
-        }
+  props: {
+    ingredientIn: {
+      index: Number,
+      name: String,
+      amount: Number,
+      unit: String,
+      caloriesPerAmount: Number,
+      totalCalories: Number
     }
+  },
+  data() {
+    return {
+      ingredient: {
+        index: this.ingredientIn.index,
+        name: this.ingredientIn.name,
+        amount: this.ingredientIn.amount,
+        unit: this.ingredientIn.unit,
+        caloriesPerAmount: this.ingredientIn.caloriesPerAmount,
+        totalCalories: this.ingredientIn.totalCalories
+      }
+    };
+  },
+  methods: {
+    calulcateCalories() {
+      if (this.ingredient.caloriesPerAmount > 0 && this.ingredient.amount > 0) {
+        this.ingredient.totalCalories = this.ingredient.caloriesPerAmount * this.ingredient.amount;
+      } else {
+        this.ingredient.totalCalories = 0;
+      }
+      this.$emit(
+        "change",
+        this.ingredient
+      );
+    }
+  }
 };
 </script>
 
 <style scoped>
 .ingredient-row {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 2rem;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 2rem;
 }
 
 .bx--form-item {
-    margin: 0 40px;
+  margin: 0 40px;
 }
 </style>
