13593b76703155573cdfd1e3817d82abfc52e158
Ruben Bratsbergm <rubenbratsberg@gmail.com>
2020-01-18 22:10:20 +0100

    Header, infobar and todos.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
-<div id="app">
+  <div id="app">
     <div>
-        <h1 class="logo">💪</h1>
-        <h1>Prep That Meal</h1>
-            <IngredientList />
+      <cv-header aria-label="Carbon header" class="header">
+        <cv-header-name href="javascript:void(0)" prefix="💪">[Prep That Meal]</cv-header-name>
+          <cv-button>Lagre</cv-button>
+      </cv-header>
+
+      <h1 class="logo">💪</h1>
+      <h1>Prep That Meal</h1>
+      <IngredientList />
+
+      <cv-toolbar class="infobar">
+        <p>Hvis totale kalorier her</p>
+      </cv-toolbar>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import IngredientList from './components/IngredientList.vue'
+import IngredientList from "./components/IngredientList.vue";
+// import Header from "./components/Header.vue";
 
 export default {
-    name: 'app',
-    components: {
-        IngredientList
-    }
-}
+  name: "app",
+  components: {
+    IngredientList
+  }
+};
 </script>
 
 <style>
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
 #meal-form {
@@ -35,12 +44,27 @@ export default {
 }
 
 .logo {
-    font-size: 6rem;
-    line-height: 8rem;
+  font-size: 6rem;
+  line-height: 8rem;
 }
 
 h1 {
-    margin: 3rem;
+  margin: 3rem;
 }
 
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.infobar {
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 0 !important;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: lightgray;
+  padding: 1rem;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div className="container">
+    <Header aria-label="IBM Platform Name">
+      <HeaderName href="#" prefix="IBM">
+        [Platform]
+      </HeaderName>
+      <HeaderGlobalBar>
+        <HeaderGlobalAction aria-label="Search">
+          <Search20 />
+        </HeaderGlobalAction>
+        <HeaderGlobalAction aria-label="Notifications" >
+          <Notification20 />
+        </HeaderGlobalAction>
+        <HeaderGlobalAction aria-label="App Switcher" >
+          <AppSwitcher20 />
+        </HeaderGlobalAction>
+      </HeaderGlobalBar>
+    </Header>
+  </div>
+</template>
+
+<script>
+import Search20 from "@carbon/icons-react/lib/search/20";
+import Notification20 from "@carbon/icons-react/lib/notification/20";
+import AppSwitcher20 from "@carbon/icons-react/lib/app-switcher/20";
+import {
+  Header,
+  HeaderName,
+  HeaderGlobalAction,
+  HeaderGlobalBar
+} from "carbon-components-react/lib/components/UIShell";
+
+import "./style.css";
+export default {
+};
+</script>
+
+<style>
+</style>
