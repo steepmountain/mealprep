a64f780413f164507f5348d8529c349fa43ad551
Ruben Bratsberg <rb@aplia.no>
2020-02-28 08:36:01 +0100

    Meal loading progress pt1

--- src/components/modals/LoadMealModal.vue
+++ src/components/modals/LoadMealModal.vue
@@ -1,8 +1,9 @@
 <template>
-  <cv-modal :visible="showModal" @modal-hidden="$emit('hide')">
+  <cv-modal :visible="showModal" @modal-hidden="$emit('hide')" @primary-click="load">
     <template slot="title">Åpne måltid</template>
     <template slot="content">
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <cv-button class="row" size="small" v-for="item in items" v-bind:key="item.id">{{item.name}}</cv-button>
     </template>
     <template slot="secondary-button">Avbryt</template>
     <template slot="primary-button">Åpne</template>
@@ -10,13 +11,26 @@
 </template>
 
 <script>
+import LocalStorageService from "../../services/LocalStorageService";
+
+const localStorageIndex = "recipes";
+const localStorageService = new LocalStorageService(localStorageIndex);
+
 export default {
   props: {
       showModal: Boolean
   },
-  methods: {
-      // https://stackoverflow.com/a/43762443 
-      // load data
+  data(){
+    return {
+      items: []
+    }
+  },
+  mounted: function() {
+    let allItems = localStorageService.load();
+    allItems.forEach(item => {
+      this.items.push(item);
+    });
+      // load data on mount and show
   }
 };
 </script>
